67a0b90deaea1582f360b6fea5be0780c4dcca12
雷育哲 <1053155915@qq.com>
2018-07-04 16:38:04 +0800

    fix(email): update emailMessage method

--- src/controller/email.controllers.ts
+++ src/controller/email.controllers.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { EmailService } from "../service/email.service";
 
 @Controller("email")
 export class EmailControllers {
   constructor(
-    private readonly emailService: EmailService,
+    @Inject(EmailService) private readonly emailService: EmailService,
   ) {}
 
   /**
@@ -13,9 +13,9 @@ export class EmailControllers {
    * @returns {Promise<{code: number; message: string}>}
    */
     @Post("emailMessage")
-    async emailMessage(@Body() data: {mContent: any, mid: number, email: [string], sender: string}): Promise<{ code: number, message:
+    async emailMessage(@Body() data: {mContent: any, mid: number, email: [string], sender: string, emailConfigId: number}): Promise<{ code: number, message:
       string }> {
-      const {mContent, mid, email, sender}  =  data;
-      return this.emailService.sendEmail(mContent, mid, email, sender);
+      const {mContent, mid, email, sender, emailConfigId}  =  data;
+      return this.emailService.sendEmail(mContent, mid, email, sender, emailConfigId);
     }
 }
