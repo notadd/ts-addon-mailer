b6ff2dbe7dc59c483ff4940c526013d81f8d48e6
雷育哲 <1053155915@qq.com>
2018-07-04 12:02:21 +0800

    feat(email): update sendEmail method

--- src/service/email.service.ts
+++ src/service/email.service.ts
@@ -4,24 +4,31 @@ import { render } from "ejs";
 import { createTransport } from "nodemailer";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { EmailConfigureEntity } from "../entity/emailConfigure.entity";
+import { EmailLogEntity } from "../entity/emailLog.entity";
 
-
+let code;
+let message;
 @Injectable()
 export class EmailService {
  constructor(
    @InjectRepository(EmailModuleEntity)
    private readonly emailModuleReq: Repository<EmailModuleEntity>,
+   @InjectRepository(EmailConfigureEntity)
+   private readonly emailConfigRep: Repository<EmailConfigureEntity>,
+   @InjectRepository(EmailLogEntity)
+   private readonly emailLogRep: Repository<EmailLogEntity>,
  ) {}
 
   /**
    * 发送邮箱
    * @param {EmailModuleEntity} emailModule 邮箱模板
    * @param {any} mContent 邮箱模板
-   * @param {EmailUserEntity[]} emailUser 收信人
+   * @param {email[]} emailUser 收信人地址
    * @param {string} sender 发信人
    * @returns {Promise<{code: number; message: string}>}
    */
-  async sendEmail(mContent: any, mid: number, email: [string], sender: string): Promise<{code, message }> {
+  async sendEmail(mContent: any, mid: number, email: [string], sender: string, emailConfigId: number): Promise<{code, message }> {
     const emailModule: EmailModuleEntity | undefined = await this.emailModuleReq.findOne(mid);
     if (!emailModule) {
       throw new HttpException("发送邮箱模板不存在", 406);
@@ -29,13 +36,18 @@ export class EmailService {
     if (email.length <= 0) {
       throw new HttpException("收件人不存在", 406);
     }
+    const emailConfig: EmailConfigureEntity | undefined = await this.emailConfigRep.findOne(emailConfigId);
+    if (emailConfig === undefined) {
+      return {code: 400, message: "短信配置文件不存在"};
+    }
+    // 可参考 https://help.aliyun.com/document_detail/29456.html?spm=a2c4g.11186623.6.606.uvABcP
     for (let i = 0; i < email.length; i++) {
       const str = `${emailModule.module}`;
       // ejs 模板渲染工具，data为参数，str为模板
       const template = render(str, mContent);
       const mailOptions = {
         // 发送人地址必须与服务器连接配置中的auth中的user属性相同
-        from: `${sender}<ibenchu-demo@yzlei.ibenchu.pw>`,
+        from: `${sender}` + `${emailConfig.fromAddress}`,
         // 收件人地址
         to: email[i],
         // 邮件主题
@@ -43,24 +55,37 @@ export class EmailService {
         // 邮件内容
         html: `${template}`,
       };
+      const emailLog: EmailLogEntity | undefined = new EmailLogEntity();
       // 服务器连接配置
       createTransport({
         // 邮箱服务器，这里是阿里云服务器
-        host: "smtpdm.aliyun.com",
+        host: emailConfig.hostAddress,
         port: 465,
         secureConnection: true,
         auth: {
-          user: "ibenchu-demo@yzlei.ibenchu.pw",
-          pass: "ibenchu123QWE",
+          user: emailConfig.authUser,
+          pass: emailConfig.authPass,
         },
       }).sendMail(mailOptions, (error, info) => {
         if (error) {
-          console.log(error);
-          return {code: 200, message: "发送失败，请稍后重试！"};
+          console.log(error.message);
+          code = 400;
+          message = "发送失败，请稍后重试！";
+          emailLog.email = email[i];
+          emailLog.code = code;
+          emailLog.message = error.message;
+          this.emailLogRep.save(emailLog);
+          return {code, message};
         }
         console.log("Message sent: " + info.response);
       });
-      return {code: 200, message: "发送成功"};
+      code = 200;
+      message = "发送成功";
+      emailLog.email = email[i];
+      emailLog.code = code;
+      emailLog.message = message;
+      await this.emailLogRep.save(emailLog);
+      return {code, message};
     }
   }
 
